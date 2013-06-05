003a7cdcdd170109ff19e89600770feb369e96cc
shaynarang <shaynarang@gmail.com>
2013-06-05 12:02:51 -0500

    created new rails project and readme, removed unnecessary files, generated task model, stopped generating fixtures

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NSSToDoList::Application.config.secret_token = '052caf219dd700099a22b9c967910a7380dd365814648f1200fc6ae4819e3efea85126c25536721b5150a236b5159baa4aeba114530135f993bcf75d6c1a5a70'
